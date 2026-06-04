c7921e2bd72084a807a50a66f2630fc0bbdc1beb
oritlevinhazan <orit.levin2@gmail.com>
2026-06-04 13:42:53 +0300

    config.js

--- data/config.js
+++ data/config.js
@@ -7,20 +7,12 @@ export default {
     password: process.env.ARBOX_USER_PASSWORD
   },
   alertzyAccountKey: process.env.ALERTZY_ACCOUNT_KEY,
-  registerTime: "21:00:00", // Exact time of registration (!important)
+  registerTime: "16:00:10",
   remindersTime: "10:00:00",
-  maxClassesPerMonth: 17,
+  maxClassesPerMonth: 9,
   // Your preffered coaches by priority on ascending order. 
   // Make sure the names appear axactly like in the Arbox app, (no need to mention all of them, only those you like)
   coach_priorities: 
-  [ 
-    "עמית גורן",
-    "רועי אופנהיימר",
-    "עומרי פלד",
-    "דניאל טנג'י",
-    "עומר לנדאו",
-    "מתן קדים",
-    "אורפז איבגי",
-  ],
+  [],
   timezone: "Asia/Jerusalem",
 };
