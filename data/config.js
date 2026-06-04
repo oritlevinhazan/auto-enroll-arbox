ad73d43fe0cb2af8e8bd092cbdd82d1da84459f8
oritlevinhazan <orit.levin2@gmail.com>
2026-06-04 17:37:51 +0300

    Update config.js

--- data/config.js
+++ data/config.js
@@ -7,7 +7,7 @@ export default {
     password: process.env.ARBOX_USER_PASSWORD
   },
   alertzyAccountKey: process.env.ALERTZY_ACCOUNT_KEY,
-  registerTime: "17:38:00",
+  registerTime: "17:40:00",
   remindersTime: "10:00:00",
   maxClassesPerMonth: 9,
   // Your preffered coaches by priority on ascending order. 
